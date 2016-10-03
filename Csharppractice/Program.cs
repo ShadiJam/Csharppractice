5521d2870fae88173260dc3ec8f384de35655be7
cog042 <cog042@gmail.com>
2016-10-03 16:29:56 -0500

    Part one done

---
+++ Csharppractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharppractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            string greeting = "Hello World";
+            int year = 1492;
+            bool havedog = false;
+            string todaysdate = "10/3/2016";
+            float money = 5.45f;
+            char x = 'X';
+
+            //string interpolation
+            Console.WriteLine(greeting);
+            Console.WriteLine("Columbus Sailed the world in " + year);
+            Console.WriteLine("It is " + havedog + " that I have a dog at home.");
+            Console.WriteLine("Today is " + todaysdate);
+            Console.WriteLine("I spent $" + money + " on a cheeseburger.");
+            Console.WriteLine(x + " marks the spot");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
